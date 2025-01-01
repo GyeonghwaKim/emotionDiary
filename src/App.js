53b609a7beb37ef7c0489af3f34790ae7c1f76f0
GyeongHwa <gyeonghwaa_@naver.com>
2025-01-01 18:10:34 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import emotion1 from "./img/emotion1.png";
+
+function App() {
+  return (
+    <div className="App">
+      <img alt="감정1" src={emotion1} />
+      </div>
+  );
+}
+
+export default App;
