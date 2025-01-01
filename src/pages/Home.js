7dc05dde8962ee1eb42ddd3f43a24a91ed2bea1b
GyeongHwa <gyeonghwaa_@naver.com>
2025-01-01 22:07:28 +0900

    button 컴포넌트 구현하기기

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,31 @@
-import { useSearchParams } from "react-router-dom";
+import Button from "../component/Button";
 
 const Home = () =>{
+//쿼리스트링 확인
+    // const [searchParams,setSearchParams]=useSearchParams();
+    // console.log(searchParams.get("sort"));
 
-    const [searchParams,setSearchParams]=useSearchParams();
-    console.log(searchParams.get("sort"));
-    return <div>Home 페이지입니다</div>
+    return( 
+        <div>
+            <Button
+                text={"기본 버튼"}
+                onClick={() =>{
+                    alert("default button")
+                }} />
+            <Button
+                type="positive"
+                text={"positive 버튼"}
+                onClick={() =>{
+                    alert("positive button")
+                }} />
+            <Button
+            type="negative"
+                text={"negative 버튼"}
+                onClick={() =>{
+                    alert("negative button")
+                }} />
+        </div>
+    );
 };
+
 export default Home;
