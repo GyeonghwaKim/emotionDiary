4b890c1fd000f0bbc096b01f8a62363f233de859
GyeongHwa <gyeonghwaa_@naver.com>
2025-01-06 23:33:43 +0900

    일기 데이터 state 만들기, 일기 state 업데이터 기능 구현하기

--- src/App.js
+++ src/App.js
@@ -4,8 +4,61 @@ import Home from './pages/Home';
 import Diary from './pages/Diary';
 import New from './pages/New';
 import Edit from './pages/Edit';
+import { useReducer } from 'react';
+
+function reducer(state,action){
+  switch(action.type){
+    case "CREATE":{
+      return [action.data,...state];
+    }
+    case "UPDATE":{
+      return state.map((it) => 
+        String(it.id) === String(action.data.id) ? {...action.data}: it);
+    }
+    case "DELETE":{
+      return state.filtier((it) => String(it.id) !== String(action.targetId));
+    }
+    default:{
+      return state;
+    }
+  }
+}
 
 function App() {
+  const [data,dispatch] = useReducer(reducer,[]);
+  const idRef=useRef(0);
+
+  const onCreate = (date,content,emotionId) => {
+    dispatch({
+      type:"CREATE",
+      data:{
+        id:idRef.current,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+    idRef.current+=1;
+  }
+
+  const onUpdate = (targetId,date,content,emotionId) => {
+    dispatch({
+      type:"UPDATE",
+      data: {
+        id:targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotionId,
+      },
+    });
+  }
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type:"DELETE",
+      targetId,
+    });
+  }
   return (
     <div className="App">
       {/*switch문 느낌 */}
@@ -16,14 +69,6 @@ function App() {
           <Route path="/diary/:id" element={<Diary />} />
           <Route path="/edit" element={<Edit />} />
         </Routes>
-        {/*페이지 이동구현*/}
-        <div>
-          <Link to={"/"}>Home</Link>
-          <Link to={"/new"}>New</Link>
-          <Link to={"/diary"}>Diary</Link>
-          <Link to={"/edit"}>Edit</Link>
-        </div>
-
     </div>
   );
 }
