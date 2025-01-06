147593f5d164ad3afe4d28a4cfc95b322326f1b8
GyeongHwa <gyeonghwaa_@naver.com>
2025-01-06 22:31:30 +0900

    감정 이미지 선택 섹션 구현하기

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,32 +1,15 @@
 import Button from "../component/Button";
+import Editor from "../component/Editor";
 import Header from "../component/Header";
 
+
 const Home = () =>{
-//쿼리스트링 확인
-    // const [searchParams,setSearchParams]=useSearchParams();
-    // console.log(searchParams.get("sort"));
 
     return( 
         <div>
-            <Header
-                title={"Home"}
-                leftChild={
-                <Button
-                type="positive"
-                text={"positive 버튼"}
-                onClick={() =>{
-                    alert("positive button")
-                }} />
-                }
-                rightChild={
-<Button
-            type="negative"
-                text={"negative 버튼"}
-                onClick={() =>{
-                    alert("negative button")
-                }} />
-                }
-                />
+            <Editor onSubmit={()=>{
+                alert("작성 완료 버튼을 클릭했음");
+            }} />
         </div>
     );
 };
